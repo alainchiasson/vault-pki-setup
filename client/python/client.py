0c69191bfd8ef9199d86f8c67d1a5e16a5962228
Alain Chiasson <alain@chaisson.org>
2022-02-27 12:23:56 -0500

    Client and server now loop

--- client/python/client.py
+++ client/python/client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import time
 
 import socket
 import ssl
@@ -10,14 +11,21 @@ server_cert = 'common.cert.pem'
 client_cert = 'client.cert.pem'
 client_key = 'client.key.pem'
 
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
-context.load_cert_chain(certfile=client_cert, keyfile=client_key)
+while True:
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
+    context.load_cert_chain(certfile=client_cert, keyfile=client_key)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn = context.wrap_socket(s, server_side=False, server_hostname=server_sni_hostname)
-conn.connect((host_addr, host_port))
-print("SSL established. Peer: {}".format(conn.getpeercert()))
-print("Sending: 'Hello, world!")
-conn.send(b"Hello, world!")
-print("Closing connection")
-conn.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        conn = context.wrap_socket(s, server_side=False, server_hostname=server_sni_hostname)
+        conn.connect((host_addr, host_port))
+        print("SSL established. Peer: {}".format(conn.getpeercert()))
+        print("Sending: 'Hello, world!")
+        conn.sendall(b"Hello, world!")
+
+        # Get message echoed back
+        data = conn.recv(1024)
+
+    print(f"Received {data!r}" )
+
+    print("Sleeping 30 seconds")
+    time.sleep(30)
